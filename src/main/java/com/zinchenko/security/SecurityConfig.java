f83d69d60466e1f0178a7d81d03e6c25ff46fdba
ArtemZinchenko <Artem.Zinchenko@livevision.com.ua>
2023-05-13 23:22:19 +0300

    disable cors

--- src/main/java/com/zinchenko/security/SecurityConfig.java
+++ src/main/java/com/zinchenko/security/SecurityConfig.java
@@ -29,6 +29,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors().disable()
                 .csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
