cadddd6d61cfb30e035fd6184344ed040292c8fc
ArtemZinchenko <Artem.Zinchenko@livevision.com.ua>
2023-05-14 02:15:31 +0300

    add test data

--- src/main/resources/db/changelog/changes/8-add-test-data.sql
+++ src/main/resources/db/changelog/changes/8-add-test-data.sql
@@ -1,2 +1,4 @@
 insert into Currencies (name, code) values ('USD', 1);
-insert into Categories (name) values ('default')
+insert into Categories (name) values ('default');
+insert into Users (email, password, first_name, last_name, role)
+values ('admin@email.com', '$2a$12$mFQItzj8oHK79kL.hBpUterCLXt6v07Rnxi.RDfB2R7W7IQqh2xxa', 'admin', 'admin', 'ADMIN');
